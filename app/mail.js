110d0e9ac2aaa4d012b3a6e501fc6fd20fbbc6d3
ilyxenc <ilya.goldobin1@gmail.com>
2022-10-03 10:03:08 +0300

    making edits

--- app/mail.js
+++ app/mail.js
@@ -1,5 +1,25 @@
 const nodemailer = require('nodemailer');
 
+const helloParams = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'hello@keytom.io',
+        pass: 'qgsodqesrcjniqsp'
+    }
+}
+
+const officeParams = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'office@keytom.io',
+        pass: 'okahwlcmeijyacvo'
+    }
+}
+
 const sendLetter = (data, subject, to) => {
 
     const transporter = nodemailer.createTransport(
