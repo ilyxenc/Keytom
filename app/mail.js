d91bd7b43e8dc17fee1726f20c69a941f8485930
ilyxenc <ilya.goldobin1@gmail.com>
2022-10-03 10:42:53 +0300

    making edits

--- app/mail.js
+++ app/mail.js
@@ -3,10 +3,6 @@ const nodemailer = require('nodemailer');
 require('dotenv').config()
 
 const helloParams = JSON.parse(process.env.helloParams)
-<<<<<<< HEAD
-
-=======
->>>>>>> 4ef5a545e92296adbe2a66b4190a0c9903502389
 const officeParams = JSON.parse(process.env.officeParams)
 
 const sendLetter = (data, subject, to) => {
